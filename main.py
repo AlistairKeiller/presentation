f3be2da9cefddf357477044c1f0c1e69a64d2bb0
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-05-10 17:16:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -62,52 +62,51 @@ class BasicExample(Slide):
         self.play(Transform(e5,e9), Transform(e2,e10))
         self.next_slide()
 
-        self.play(FadeOut(e1),FadeOut(e2),FadeOut(e5))
-        e9 = MathTex(r"a+b=1,\ \alpha b = \frac{1}{2},\ \beta b = \frac{1}{2}", color=YELLOW).next_to(e0,DOWN)
-        self.play(Write(e9))
-        self.next_slide()
-
-        e10 = MathTex(r"&\text{Euler Solution:}{{ \\ &a=b=\frac{1}{2} \\ &\alpha=\beta=1}}", color=RED).next_to(e9,DOWN).to_edge(LEFT)
-        self.play(Write(e10))
-        self.next_slide()
-
-        e11 = MathTex(r"\text{Midpoint Solution:}{{& \\ a=0& \\ b=1& \\ \alpha=\beta=\frac{1}{2}&}}", color=GREEN).next_to(e9,DOWN).to_edge(RIGHT)
+        self.play(FadeOut(e1,e2,e5))
+        e11 = MathTex(r"a+b=1,\ \alpha b = \frac{1}{2},\ \beta b = \frac{1}{2}", color=YELLOW).next_to(e0,DOWN)
         self.play(Write(e11))
         self.next_slide()
 
-        e12 = MathTex(r"&\text{Euler Solution:}{{ \\ &k_1=\Delta t f(t,y) \\ &k_2=\Delta t f(t+\Delta t, y+k_1) \\ &y(t+\Delta t)=y+\frac{1}{2}(k_1+k_2)}}", color=RED).next_to(e9,DOWN).to_edge(LEFT)
-        self.play(Transform(e10,e12))
+        e12 = MathTex(r"&\text{Euler Solution:}{{ \\ &a=b=\frac{1}{2} \\ &\alpha=\beta=1}}", color=RED).next_to(e11,DOWN).to_edge(LEFT)
+        self.play(Write(e12))
         self.next_slide()
 
-        e13 = MathTex(r"\text{Midpoint Solution:}{{& \\ k_1=\Delta t f(t,y)& \\ k_2=\Delta t f(t+\frac{1}{2} \Delta t, y+\frac{1}{2} k_1)& \\ y(t+\Delta t)=y+k_2&}}", color=GREEN).next_to(e9, DOWN).to_edge(RIGHT)
-        self.play(Transform(e11,e13))
+        e13 = MathTex(r"\text{Midpoint Solution:}{{& \\ a=0& \\ b=1& \\ \alpha=\beta=\frac{1}{2}&}}", color=GREEN).next_to(e11,DOWN).to_edge(RIGHT)
+        self.play(Write(e13))
         self.next_slide()
 
-        self.play(FadeOut(e0), FadeOut(e9), FadeOut(e10), FadeOut(e11))
-        e14 = Text("Case Study: Newtonian Drag").to_edge(UP)
-        self.play(Write(e14))
-        e15 = MathTex(r"x''&=\mu x' \sqrt{x'^2+y'^2+z'^2} \\ y''&=\mu y' \sqrt{x'^2+y'^2+z'^2} \\ z''&=\mu z' \sqrt{x'^2+y'^2+z'^2}")
-        self.play(Write(e15))
-
-        self.next_slide()
-        e16 = MathTex(r"a_x&=\mu v_x \sqrt{v_x^2+v_y^2+v_z^2} \\ a_y&=\mu v_y \sqrt{v_x^2+v_y^2+v_z^2} \\ a_z&=\mu v_z \sqrt{v_x^2+v_y^2+v_z^2}{{ \\ x'&=v_x \\ y'&=v_y \\ z'&=v_z}}")
-        self.play(Transform(e15,e16))
+        e14 = MathTex(r"&\text{Euler Solution:}{{ \\ &k_1=\Delta t f(t,y) \\ &k_2=\Delta t f(t+\Delta t, y+k_1) \\ &y(t+\Delta t)=y+\frac{1}{2}(k_1+k_2)}}", color=RED).next_to(e11,DOWN).to_edge(LEFT)
+        self.play(Transform(e12,e14))
         self.next_slide()
 
-        self.play(FadeOut(e14,e15))
-        e17 = Text("Case Study: Schrodinger Equation").to_edge(UP)
+        e15 = MathTex(r"\text{Midpoint Solution:}{{& \\ k_1=\Delta t f(t,y)& \\ k_2=\Delta t f(t+\frac{1}{2} \Delta t, y+\frac{1}{2} k_1)& \\ y(t+\Delta t)=y+k_2&}}", color=GREEN).next_to(e11, DOWN).to_edge(RIGHT)
+        self.play(Transform(e13,e15))
+        self.next_slide()
+
+        self.play(FadeOut(e0,e11,e12,e13))
+        e16 = Text("Case Study: Newtonian Drag").to_edge(UP)
+        self.play(Write(e16))
+        e17 = MathTex(r"x''&=\mu x' \sqrt{x'^2+y'^2+z'^2} \\ y''&=\mu y' \sqrt{x'^2+y'^2+z'^2} \\ z''&=\mu z' \sqrt{x'^2+y'^2+z'^2}")
         self.play(Write(e17))
-        e18 = MathTex(r"\frac{\partial \Psi}{\partial t}={{}-i h H}} \Psi")
-        self.play(Write(e18))
+
+        self.next_slide()
+        e18 = MathTex(r"a_x&=\mu v_x \sqrt{v_x^2+v_y^2+v_z^2} \\ a_y&=\mu v_y \sqrt{v_x^2+v_y^2+v_z^2} \\ a_z&=\mu v_z \sqrt{v_x^2+v_y^2+v_z^2}{{ \\ x'&=v_x \\ y'&=v_y \\ z'&=v_z}}")
+        self.play(Transform(e17,e18))
         self.next_slide()
 
-        e19 = MathTex(r"\frac{\partial \Psi}{\partial t}={{-i h (V + \nabla^2)}} \Psi")
-        self.play(Transform(e18,e19))
+        self.play(FadeOut(e16,e17))
+        e19 = Text("Case Study: Schrodinger Equation").to_edge(UP)
+        self.play(Write(e19))
+        e20 = MathTex(r"\frac{\partial \Psi}{\partial t}={{}-i h H}} \Psi")
+        self.play(Write(e20))
+        self.next_slide()
+
+        e21 = MathTex(r"\frac{\partial \Psi}{\partial t}={{-i h (V + \nabla^2)}} \Psi")
+        self.play(Transform(e20,e21))
         self.next_slide()
         
-        e20 = MathTex(r"\frac{\partial \Psi}{\partial t}={{-i h (V + \frac{\partial^2}{\partial^2 x} + \frac{\partial^2}{\partial^2 y})}} \Psi")
-        self.play(Transform(e18,e20))
+        e22 = MathTex(r"\frac{\partial \Psi}{\partial t}={{-i h (V + \frac{\partial^2}{\partial^2 x} + \frac{\partial^2}{\partial^2 y})}} \Psi")
+        self.play(Transform(e20,e22))
         self.next_slide()
 
-        e
         
